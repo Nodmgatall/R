e05e7c1f51c3e6ebc65d4dcff51f03f18c7e17eb
NodmGatall <oliverheidmann@hotmail.de>
2016-05-18 16:47:07 +0200

    inital commit and first working package functions with parsing vectors, ints, lists between C++ and R/Rcpp

---
+++ rcpp_test.r
@@ -0,0 +1,20 @@
+library(Rcpp)
+library(test)
+
+rcpp_hello_world()
+x = 100
+x
+return_given_value(x)
+ i = c(1,2,3)
+j = c(1,4,6, 22)
+
+add_lists(i,j)
+
+#cppFunction('double add(double x, double y, double z)
+#{
+#    std::cout << "lol" << std::endl;
+#    double sum = x + y + z;
+#    return sum;
+#}')
+
+#add(2,33,24.3)
