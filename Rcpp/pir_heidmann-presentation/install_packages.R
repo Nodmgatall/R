1a3eae2c23cc027b65e871ef856a2a96710d31e8
NodmGatall <oliverheidmann@hotmail.de>
2016-06-15 08:45:16 +0200

    added script for installing packages

---
+++ Rcpp/pir_heidmann-presentation/install_packages.R
@@ -0,0 +1,6 @@
+
+repo <- "https://cran.uni-muenster.de/"
+install.packages("Rcpp", repos =repo)
+install.packages("ggplot2", repos = repo)
+install.packages("microbenchmark", repos = repo)
+install.packages("inline", repos = repo)
